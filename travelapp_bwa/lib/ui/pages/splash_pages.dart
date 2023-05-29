6fc7c5baf038b50d6c6230411a9fcc039d928690
RioFahlevi <riofahlevi2011@gmail.com>
2023-05-29 10:47:32 +0700

    update day1

---
+++ travelapp_bwa/lib/ui/pages/splash_pages.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:travelapp_bwa/ui/pages/get_started_pages.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    setState(() {
+      super.initState();
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GetStartedPage(),
+            ));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(bottom: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icon_plane.png'),
+                ),
+              ),
+            ),
+            Text(
+              'AIRPLANE',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
